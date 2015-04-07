4fd19965e63a268c02311b534ae92404b2b9e3b5
Semen Danilov <samvimes@yandex.ru>
2015-04-07 22:59:00 +0300

    asd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("TempFolder: " + Properties.BASE_DOWNLOAD_URI);
         CommandThread commandThread = new CommandThread();
         commandThread.start();
     }
--- src/com/company/Properties.java
+++ src/com/company/Properties.java
@@ -7,7 +7,7 @@ public class Properties {
 
     public static final String USER_NAME = "TEST";
     public static final String PASSWORD = "TEST";
-    public static final String BASE_DOWNLOAD_URI = "C:\\myFiles\\";
+    public static final String BASE_DOWNLOAD_URI = System.getProperty("java.io.tmpdir");;
 
     public static final String HOST = "188.225.77.104";
     public static final int PORT = 8800;
